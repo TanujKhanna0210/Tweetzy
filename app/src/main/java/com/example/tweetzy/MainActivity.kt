68f9744f90f0594b577eddd46e4524f435f62318
Tanuj Khanna <tanujkhanna72@gmail.com>
2023-12-17 17:00:18 +0530

    Creating detail screen composables

--- app/src/main/java/com/example/tweetzy/MainActivity.kt
+++ app/src/main/java/com/example/tweetzy/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.tweetzy
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.example.tweetzy.screens.CategoryScreen
+import com.example.tweetzy.screens.DetailScreen
 import com.example.tweetzy.ui.theme.TweetzyTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -14,7 +14,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TweetzyTheme {
-                CategoryScreen()
+                DetailScreen()
             }
         }
     }
